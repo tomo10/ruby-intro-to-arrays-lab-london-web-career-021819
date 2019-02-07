f714cfe86f0694836b0acac853a26e3adb9d8dad
Thomas Edwards <thce28@gmail.com>
2019-02-07 16:48:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,3 +1,23 @@
 def instantiate_new_array
   []
 end
+
+def array_with_two_elements
+  []
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
+def first_element_with_array(array)
+  
+  
